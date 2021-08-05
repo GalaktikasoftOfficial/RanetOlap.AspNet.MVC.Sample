ce3645a4f49bef5c66ae3180bf15e24c70346234
Petr Leschenok <petr.leschenok@galaktika-soft.com>
2021-08-05 17:46:07 +0300

    update sample solution

--- Ranet.PivotTable.MVC/Settings.cs
+++ Ranet.PivotTable.MVC/Settings.cs
@@ -2,15 +2,15 @@
 
 namespace Ranet.PivotTable.MVC
 {
-	public class Settings : IDynamicPivotGridQueryDesignerSettings
-	{
-		public bool GenerateCustomCalculated { get; set; }
-		public bool HideEmptyColumns { get; set; }
-		public bool HideEmptyRows { get; set; }
-		public bool IncludeAllMembers { get; set; }
-		public bool IncludeCustomCalculationsInDrillDown { get; set; }
-		public int SubsetCount { get; set; }
-		public bool UseVisualTotals { get; set; }
-		public bool UseHierarchize { get; set; }
-	}
+    public class Settings : IDynamicPivotGridQueryDesignerSettings
+    {
+        public bool GenerateCustomCalculated { get; set; }
+        public bool HideEmptyColumns { get; set; }
+        public bool HideEmptyRows { get; set; }
+        public bool IncludeAllMembers { get; set; }
+        public bool IncludeCustomCalculationsInDrillDown { get; set; }
+        public int SubsetCount { get; set; }
+        public bool UseVisualTotals { get; set; }
+        public bool UseHierarchize { get; set; }
+    }
 }
